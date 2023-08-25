f9af6e311fd474f7c7cd73d447bfdcd8981761cd
balijawa hussein <balijawahussein@gmail.com>
2023-08-25 08:04:55 +0300

    i have made changes @hussein

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const addingHtmlElementsJs=()=>
 {
     let root = document.getElementsByClassName("root")[0];
+    let header = document.createElement("header");
     let nav = document.createElement("nav");
     let li1 = document.createElement("li");
     let an = document.createElement("a");
-    li1.textContent ="home";
+    an.textContent ="home";
     nav.appendChild(li1);
-    root.appendChild(nav);
+    li1.appendChild(an);
+    root.appendChild(header);
+    header.appendChild(nav);
 }
 addingHtmlElementsJs();
