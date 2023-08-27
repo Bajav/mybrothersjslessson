5688a6dc8220c49aff5177c7cd6142a5b6dfaa2b
balijawa hussein <balijawahussein@gmail.com>
2023-08-27 12:57:41 +0300

    here is a new update

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const addingHtmlElementsJs=()=>
-{
-    let root = document.getElementsByClassName("root")[0];
-    let header = document.createElement("header");
-    let nav = document.createElement("nav");
-    let li1 = document.createElement("li");
-    let an = document.createElement("a");
-    an.textContent ="home";
-    nav.appendChild(li1);
-    li1.appendChild(an);
-    root.appendChild(header);
-    header.appendChild(nav);
-}
-addingHtmlElementsJs();
+// const addingHtmlElementsJs=()=>
+// {
+//     let root = document.getElementsByClassName("root")[0];
+//     let header = document.createElement("header");
+//     let nav = document.createElement("nav");
+//     let li1 = document.createElement("li");
+//     let an = document.createElement("a");
+//     an.textContent ="home";
+//     nav.appendChild(li1);
+//     li1.appendChild(an);
+//     root.appendChild(header);
+//     header.appendChild(nav);
+// }
+// addingHtmlElementsJs();
